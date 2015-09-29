81cf6e3a39e55928b6f4b3969b8b35eab69590a2
Sebastian Florez <sfrsebastian@gmail.com>
2015-09-28 23:14:05 -0500

    corregido TCP

--- App/Lab6Redes/app/src/main/java/com/labredes/grupo15/lab6redes/SenderTCP.java
+++ App/Lab6Redes/app/src/main/java/com/labredes/grupo15/lab6redes/SenderTCP.java
@@ -24,45 +24,33 @@ class SenderTCP extends AsyncTask<Integer, Void, String> {
 
 
     protected String doInBackground(Integer... repeticiones) {
-
-        for( int i = 0 ; i < repeticiones[0].intValue() ; i++) {
-            try {
-                Socket socket = new Socket("157.253.220.83", 5006);
-
-                InputStream in = socket.getInputStream();
-                OutputStream out = socket.getOutputStream();
-                PrintWriter output = new PrintWriter(out);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-                Location location = buffer.getLocation();
-                String message = i + "," + location.getLatitude() + "," + location.getLongitude() + "," + location.getAltitude() + "," + location.getSpeed();
-
-                output.println("HELLO");
-
-                Log.i("TCP", "Ubicación: " + message);
-                output.println(message);
-
+        try {
+            Socket socket = new Socket("192.168.0.17", 5006);
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+            PrintWriter output = new PrintWriter(out,true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            output.println("HELLO");
+            if (reader.readLine().equals("HELLO")) {
+                for (int i = 0; i < repeticiones[0].intValue(); i++) {
+                    Location location = buffer.getLocation();
+                    String message = i + "," + location.getLatitude() + "," + location.getLongitude() + "," + location.getAltitude() + "," + location.getSpeed();
+                    Log.i("TCP", "Ubicación: " + message);
+                    output.println(message);
+                    Log.i("TCP", "Flush!");
+                    TimeUnit.SECONDS.sleep(1);
+                }
                 output.println("GOODBYE");
-
-                out.flush();
                 out.close();
                 socket.close();
-
-                TimeUnit.SECONDS.sleep(1);
-
-            } catch (UnknownHostException e){
-                Log.e("UnknownHostException", e.getMessage());
-
-            } catch (IOException e){
-                Log.e("IOException", e.getMessage());
-
-            } catch (InterruptedException e){
-                Log.e("InterruptedException", e.getMessage());
-
+                return "Ok";
             }
         }
+        catch (Exception e){
+            Log.e("UnknownHostException", e.getMessage());
 
-        return "Ok";
+        }
+        return "ok";
     }
 
 }
